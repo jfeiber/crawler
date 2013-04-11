119eb69bab1a3ecf4c7a4fcc52795b38be3caeac
Josh Feiber <joshua@feiber.com>
2013-04-11 16:17:56 -0700

    Added a database where the HTML and URL are stored.

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 var express = require("express");
 var app = express();
 var request = require("request");
-var Diffbot = require('diffbot').Diffbot
+var Diffbot = require('diffbot').Diffbot;
+var pg = require('pg').native;
+
+DATABASE_URL = 'postgres://yvjcpskpyvougu:AAjg7EhphZtmcLAJ2UQgYrtaiX@ec2-54-243-224-162.compute-1.amazonaws.com:5432/dchofrh486l0gi';
 
 app.use(express.logger());
 
-var diffbot = new Diffbot('a425f0fde637e1f7741e20c2cfdae71a')
+var diffbot = new Diffbot('a425f0fde637e1f7741e20c2cfdae71a');
 
 app.get('/', function (req, res){
 	res.setHeader('Content-Type', 'json');
@@ -26,7 +29,12 @@ app.get('/crawl', function (req, res){
 			res.end(JSON.stringify({"isHTML" : "False", "errorCode" : '400'}));
 		} else if (response.statusCode == 200){
 			diffbot.article({uri: siteToCrawl, html: true}, function(err, responseDiffbot) {
-				res.end(JSON.stringify({"isHTML": "True", "rawHTML" : responseDiffbot.html})); 
+				res.end(JSON.stringify({"isHTML": "True", "rawHTML" : responseDiffbot.html}));
+				pg.connect(DATABASE_URL, function (err, client){
+					if (err)
+						console.log(err);
+					var query = client.query('INSERT INTO crawldata (url, html) VALUES (\''+siteToCrawl+'\', \''+responseDiffbot.html+'\');');
+				}); 
 			});
 		} else{
 			res.end(JSON.stringify({"isHTML" : "False", "errorCode" : response.statusCode}));
