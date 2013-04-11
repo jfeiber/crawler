f1d426d80f5325a1befc278dfb83624d4e703a11
Josh Feiber <joshua@feiber.com>
2013-04-11 09:42:37 -0700

    Now using DiffBot to parse the HTML. Changed the structure of the URL to use a query.

--- app.js
+++ app.js
@@ -1,18 +1,39 @@
 var express = require("express");
 var app = express();
 var request = require("request");
+var Diffbot = require('diffbot').Diffbot
+
+var diffbot = new Diffbot('a425f0fde637e1f7741e20c2cfdae71a')
 
 app.get('/crawl', function (req, res){
-	request(req.query.site, function (err, response, body){
+	var siteToCrawl = req.query.site;
+	request(siteToCrawl, function(err, response, body){
 		res.setHeader('Content-Type', 'json');
 		if (!err && response.statusCode == 200){
-			var jsonResponse = JSON.stringify({"isHTML": "True", "rawHTML" : body}); 
+			diffbot.article({uri: siteToCrawl, html: true}, function(err, responseDiffbot) {
+				if (err)
+					console.log(err);
+				var jsonResponse = JSON.stringify({"isHTML": "True", "rawHTML" : responseDiffbot.html});
+				res.end(jsonResponse); 
+			});
+		} else{
+			jsonResponse = JSON.stringify({"isHTML" : "False", "errorCode" : response.statusCode});
+			res.end(jsonResponse);
 		}
-		else{
-			var jsonResponse = JSON.stringify({"isHTML" : "False", "errorCode" : response.statusCode});
-		}
-		res.end(jsonResponse);
 	});
 });
 
 app.listen(5000);
+
+
+//Old Request Code:
+// request(req.query.site, function (err, response, body){
+// 	res.setHeader('Content-Type', 'json');
+// 	if (!err && response.statusCode == 200){
+// 		var jsonResponse = JSON.stringify({"isHTML": "True", "rawHTML" : body}); 
+// 	}
+// 	else{
+// 		var jsonResponse = JSON.stringify({"isHTML" : "False", "errorCode" : response.statusCode});
+// 	}
+// 	res.end(jsonResponse);
+// });
